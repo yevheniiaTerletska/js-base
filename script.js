4d353e6b9c022182079a5d800208702d2a9ee3ae
Brovchuk <brovchuk2013@gmail.com>
2020-05-12 14:19:41 +0300

    homework 3

--- script.js
+++ script.js
@@ -1,4 +1,74 @@
-//Щоб перевірити - потрібно розкоментувати виклик відповідної функції
+//HOMEWORK 3
+
+// 1. Basic Algorithm Scripting: Convert Celsius to FahrenheitPassed
+function convertToF(celsius) {
+  var fahrenheit;
+  fahrenheit =  (celsius * 9 / 5 + 32);
+  return fahrenheit;
+}
+// convertToF(30);
+
+// 2. Reverse a String (раздел Basic Algorithm Scripting)
+function reverseString(str) {
+  var arr = str.split('');
+  var reversed = arr.reverse();
+  str = reversed.join('');
+  return str;
+}
+
+// reverseString("hello");
+
+// 3. Basic Algorithm Scripting: Repeat a String Repeat a String
+
+function repeatStringNumTimes(str, num) {
+  if(num <= 0 ) {
+    str = '';
+  } else {
+    var times = 1;
+    var strAdded = str;
+    for (times; times < num; times+=1){
+      str +=strAdded;
+    }
+  }
+  return str;
+}
+repeatStringNumTimes("Abc", -8);
+
+// 4. Создайте массив arr с числовыми величинами 45,78,10,3.
+// Добавьте в массив arr еще одно число.
+// Посчитайте в переменную sum сумму чисел в нем, при помощи цикла for/while.
+
+var arr = [45, 78, 10, 3];
+var newNumber = arr.push(47);
+function arraySum(arr){
+  var sum = 0;
+  var index = 0;
+  for(index; index < arr.length; index+=1){
+    sum += arr[index];
+  }
+  // console.log(sum);
+}
+arraySum(arr);
+
+//5. .Дан объект Напишите код для суммирования всех зарплат и сохраните результат в переменной sum.
+
+var salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+var sum = 0;
+var prop;
+for (prop in salaries){
+  sum +=salaries[prop];
+}
+// console.log(sum);
+
+var salary = 'salary';
+console.log(salary.length);
+
+
+//HOMEWORK 2
 
 //1. Требуется написать функцию, выводящую в консоль числа от 1 до n, где n — это целое число, которая функция принимает в качестве параметра, с такими условиями:
 //вывод fizz вместо чисел, кратных 3;
@@ -8,18 +78,19 @@
 function swowInterval() {
   var n = 60;
   var i;
-  for (i=1; i<=n; i+=1) {
-    if (i%3 === 0 && i%5 === 0) {
+
+  for (i = 1; i <= n; i +=1) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
-      continue;
-    } else if (i%3 === 0){
-      console.log("fizz");
-      continue;
-    } else if (i%5 === 0){
-      console.log("buzz");
-      continue;
     }
-    console.log(i);
+    else if (i % 3 === 0){
+      console.log("fizz");
+    }
+    else if (i % 5 === 0){
+      console.log("buzz");
+    }
+
+    console.log( i );
   }
 }
 // swowInterval();
@@ -30,10 +101,10 @@ function showMultipleNumbers() {
   var numStart = 45;
   var numEnd = 670;
   while (numStart <= numEnd) {
-    if(numStart%10 === 0){
-      console.log(numStart)
+    if(numStart % 10 === 0){
+      console.log( numStart )
     }
-    numStart+=1;
+    numStart +=1;
   }
 }
 // showMultipleNumbers();
@@ -43,9 +114,9 @@ function showMultipleNumbers() {
 function showNumbers() {
   var numStart = 45;
   var numEnd = 67;
-  while (numStart<=numEnd) {
-    console.log(numStart);
-    numStart+=1;
+  while (numStart <= numEnd) {
+    console.log( numStart );
+    numStart +=1;
   }
 }
 // showNumbers();
@@ -58,12 +129,11 @@ function showCommon() {
   var numSecond = 670;
 
   for (numStart; numStart<=numSecond;) {
-    if (numStart<=numFirst){
-      console.log(numStart);
-      numStart += 1;
-      continue;
-    } else if (numStart%10 === 0) {
-      console.log(numStart);
+    if (numStart <= numFirst){
+      console.log( numStart );
+    }
+    else if ( numStart % 10 === 0) {
+      console.log( numStart );
     }
     numStart+=1;
   }
@@ -110,3 +180,44 @@ function swowSimpleNumbers() {
     }
 }
 // swowSimpleNumbers();
+
+//TO DO
+// var a = 2;
+// function fn() {
+//   console.log(a);
+//   var  b = 3;
+//   console.log(b);
+// }
+// // fn();
+
+// var object = {
+//   name: "me",
+//   age: 23,
+//   isAdmin: true
+//
+// };
+//
+// object.isAdmin = false; //update
+// delete object.age; //delete
+// object.country = 'ukraine';
+// object['city'] = 'kyiv';
+//
+// console.log(object);
+//
+// for (prop in object) {
+//   if(typeof object[prop] === 'string'){
+//     delete object[prop];
+//   }
+//
+// }
+// console.log(object[prop]);
+//
+// function multiply(a, b) {
+//   return a * b;
+// }
+//
+// let result = multiply(1, 2);
+// console.log( result ); // 3
+
+
+
